8907701da7dcf49a90f10acba182a9f63b015785
subhro hazra <subhrohazra7@gmail.com>
2025-10-14 22:46:04 +0530

    .

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -2,38 +2,81 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./BookForm.css";
 
-
-
-
-const BookForm = ({addBook}) => {
+const BookForm = ({ addBook }) => {
   const navigate = useNavigate();
- 
-  const [book, setBook] = useState({ name: "",author: "", publishDate: "",description: "",price: 0,img: "",});
 
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [genre, setGenre] = useState("");
+  const [price, setPrice] = useState(0);
+  const [publishDate, setPublishDate] = useState();
+  const [description, setDescription] = useState("");
+  const [img, setImg] = useState("");
 
-
-  const handleChange = (e) => {
-    setBook({ ...book, [e.target.name]: e.target.value });
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
 
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
+  const handlePriceChange = (e) => {
+    setPrice(e.target.value);
+  };
+  const handlePublishDateChange = (e) => {
+    setPublishDate(e.target.value);
+  };
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleImgChange = (e) => {
+    setImg(e.target.value);
+  };
+
+  let book = {
+    name,
+    author,
+    genre,
+    publishDate,
+    description,
+    price: Number(price),
+    img,
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if ( !book.name || !book.author ||!book.price ||  !book.description ||!book.publishDate || !book.genre || !book.img )
+    if (
+      !book.name ||
+      !book.author ||
+      !book.price ||
+      !book.description ||
+      !book.publishDate ||
+      !book.genre ||
+      !book.img
+    )
       return alert("All fields required!");
 
-/*---------------------->date validation<---------------------------------- */
+    /*---------------------->price validation<---------------------------------- */
 
-  const today = new Date().toISOString(); 
-  if (book.publishDate === today) {
-    return alert("Publication date cannot be today!");
-  }
+    if (isNaN(price) || price < 0) {
+      alert(" Price must be a valid positive number.");
+      return;
+    }
 
+    /*---------------------->date validation<---------------------------------- */
+    const today = new Date().toISOString();
+    if (book.publishDate === today) {
+      return alert("Publication date cannot be today!");
+    }
 
-  /*-------------------------->sending Data<-------------------------- */
+    /*-------------------------->sending Data<-------------------------- */
     try {
-
       const res = await fetch(
         "https://book-inventory-8c66d-default-rtdb.asia-southeast1.firebasedatabase.app/book.json",
         {
@@ -44,38 +87,80 @@ const BookForm = ({addBook}) => {
       );
 
       const data = await res.json();
-      console.log("Book added:",data.name);
+      console.log("Book added:", data.name);
       const newBook = { id: data.name, ...book };
       addBook(newBook);
       navigate("/");
-      setBook({ name: "", author: "", publishDate: "",  description: "",img: "", price: 0, });
-
+      setBook({
+        name: "",
+        author: "",
+        publishDate: "",
+        description: "",
+        img: "",
+        price: 0,
+      });
     } catch (error) {
       console.error("Error adding book:", error);
     }
-
-
-
-
   };
 
   return (
     <div className="book">
-
       <h2 className="title">Add a New Book</h2>
 
       <form className="book-form" onSubmit={handleSubmit}>
-
-        <input type="text" name="name" placeholder="Book name" value={book.name} onChange={handleChange} />
-        <input type="text" name="author" placeholder="Author" value={book.author} onChange={handleChange} />
-        <input type="text" name="genre" placeholder="genre" value={book.genre} onChange={handleChange} />
-        <input type="number"name="price" placeholder="price" value={book.price} onChange={handleChange} />
-        <input type="date" name="publishDate" placeholder="Enter The Date" className="dateInp"  value={book.publishDate} onChange={handleChange} />
-        <input type="text" name="description" placeholder="Description" value={book.description} onChange={handleChange}  />
-        <input type="text" name="img" placeholder="img" value={book.img} onChange={handleChange} />
+        <input
+          type="text"
+          name="name"
+          placeholder="Book name"
+          value={book.name}
+          onChange={handleNameChange}
+        />
+        <input
+          type="text"
+          name="author"
+          placeholder="Author"
+          value={book.author}
+          onChange={handleAuthorChange}
+        />
+        <input
+          type="text"
+          name="genre"
+          placeholder="genre"
+          value={book.genre}
+          onChange={handleGenreChange}
+        />
+        <input
+          type="number"
+          name="price"
+          placeholder="price"
+          value={book.price}
+          onChange={handlePriceChange}
+        />
+        <input
+          type="date"
+          name="publishDate"
+          placeholder="Enter The Date"
+          className="dateInp"
+          value={book.publishDate}
+          onChange={handlePublishDateChange}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={book.description}
+          onChange={handleDescriptionChange}
+        />
+        <input
+          type="text"
+          name="img"
+          placeholder="img"
+          value={book.img}
+          onChange={handleImgChange}
+        />
 
         <button type="submit">Add Book</button>
-
       </form>
     </div>
   );
--- src/components/EditBook.jsx
+++ src/components/EditBook.jsx
@@ -3,26 +3,84 @@ import { useParams, useNavigate } from "react-router-dom";
 import "./EditBook.css"
 
 const EditBook = ({ books, updateBook }) => {
-  
+
   const { id } = useParams();
   const navigate = useNavigate();
 
   const book = books.find((b) => b.id === id);
 
-  const [updatedBook, setUpdatedBook] = useState({ name: "", author: "", genre: "", publishDate: "", description: "", price: 0,img: "",});
+
+
+  const[name,setName]=useState("");
+  const[author,setAuthor]=useState("");
+  const[genre,setGenre]=useState("");
+  const[price,setPrice]=useState(0);
+  const[publishDate,setPublishDate]=useState();
+  const[description,setDescription]=useState("");
+  const[img,setImg]=useState("");
+
 
   useEffect(() => {
     if (book) {
-      setUpdatedBook(book);
+      setName(book.name);
+      setAuthor(book.author);
+      setGenre(book.genre);
+      setPrice(book.price);
+      setPublishDate(book.publishDate);
+      setDescription(book.description);
+      setImg(book.img);
     }
   }, [book]);
 
-  const handleChange = (e) => {
-    setUpdatedBook({ ...updatedBook, [e.target.name]: e.target.value });
-  };
+
+const handleNameChange=(e)=>{
+setName(e.target.value)
+};
+
+const handleAuthorChange=(e)=>{
+setAuthor(e.target.value)
+}
+const handleGenreChange=(e)=>{
+setGenre(e.target.value)
+}
+
+const handlePriceChange=(e)=>{
+setPrice(e.target.value)
+}
+const handlePublishDateChange=(e)=>{
+setPublishDate(e.target.value)
+}
+const handleDescriptionChange=(e)=>{
+setDescription(e.target.value)
+}
+
+const handleImgChange=(e)=>{
+setImg(e.target.value)
+}
+
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (isNaN(price) || price < 0) {
+      alert(" Price must be a valid positive number.");
+      return;
+    }
+
+
+   const updatedBook = {
+      id: book.id,
+      name,
+      author,
+      genre,
+      price: Number(price),
+      publishDate,
+      description,
+      img,
+    };
+
+
     try {
       const res = await fetch(
         `https://book-inventory-8c66d-default-rtdb.asia-southeast1.firebasedatabase.app/book/${book.id}.json`,
@@ -54,13 +112,13 @@ const EditBook = ({ books, updateBook }) => {
 
       <form className="book-form" onSubmit={handleSubmit}>
 
-        <input type="text" name="name" placeholder="Book name" value={updatedBook.name} onChange={handleChange} />
-        <input type="text" name="author" placeholder="Author"  value={updatedBook.author}  onChange={handleChange}/>
-        <input type="text" name="genre"  placeholder="Genre" value={updatedBook.genre} onChange={handleChange}/>
-        <input type="number" name="price" placeholder="Price" value={updatedBook.price} onChange={handleChange} />
-        <input type="date"name="publishDate"  className="eDateInp"value={updatedBook.publishDate}onChange={handleChange} />
-        <input type="text" name="description"placeholder="Description"value={updatedBook.description}onChange={handleChange} />
-        <input type="text" name="img" placeholder="Image URL" value={updatedBook.img} onChange={handleChange} />
+        <input type="text" name="name" placeholder="Book name" value={name} onChange={handleNameChange} />
+        <input type="text" name="author" placeholder="Author"  value={author}  onChange={handleAuthorChange}/>
+        <input type="text" name="genre"  placeholder="Genre" value={genre} onChange={handleGenreChange}/>
+        <input type="number" name="price" placeholder="Price" value={price} onChange={handlePriceChange} />
+        <input type="date"name="publishDate"  className="eDateInp"value={publishDate}onChange={handlePublishDateChange} />
+        <input type="text" name="description"placeholder="Description"value={description}onChange={handleDescriptionChange} />
+        <input type="text" name="img" placeholder="Image URL" value={img} onChange={handleImgChange} />
         <button type="submit">Update Book</button>
       </form>
     </div>
